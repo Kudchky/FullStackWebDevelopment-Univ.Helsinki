6c156f9e9c7bf6a170e717f16298824918739194
Kudchky <rpcastilloy@gmail.com>
2023-01-25 08:09:42 -0500

    Done part1c

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Content from "./Content";
 import Header from "./Header";
-import Total from "./Total.jsx";
+import Total from "./Total";
+import Display from "./Display";
+import Button from "./Button.jsx";
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  /* setTimeout(() => setCounter(counter + 1), 1000); */
+  const increaseByOne = () => setCounter(counter + 1);
+  const setToZero = () => setCounter(0);
+  const decrementByOne = () => setCounter(counter - 1);
+
   const course = {
     name: "Half Stack application development",
     parts: [
@@ -24,11 +33,18 @@ const App = () => {
 
   return (
     <div>
+      <Display counter={counter} />
       <Header course={course.name} />
       <Content parts={course.parts} />
       <Total total={course.parts} />
+      <Button handleClick={increaseByOne} text="Plus" />
+      <br />
+      <Button handleClick={setToZero} text="Reset" />
+      <br />
+      <Button handleClick={() => setCounter(0)} text="Zero" />
+      <br />
+      <Button handleClick={decrementByOne} text="Decrement" />
     </div>
   );
 };
-
 export default App;
